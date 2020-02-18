b968ab3c2cc2315fd88ffd7639b9e4501959679a
Jorge <jorgemichelena@Richars.localdomain>
2020-02-18 18:15:04 -0300

    add login endpoint

--- target/settings.py
+++ target/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
+    'users',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'target.wsgi.application'
 
+AUTH_USER_MODEL = 'users.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
