0cca6babfc6fcb1417c56d4aa170414dee102b99
Jorge Michelena <jorge.michelena@rootstrap.com>
2020-04-08 17:25:00 -0300

    Correct code style of targets/admin.py using flake8

--- targets/admin.py
+++ targets/admin.py
@@ -2,19 +2,24 @@ from django.contrib.gis import admin
 from targets.models import Topic, Target
 from users.models import User
 
+
 class TopicAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Name', {'fields': ['name']}),
     ]
     list_display = ['name']
     list_filter = ['name']
+
+
 class TargetAdmin(admin.ModelAdmin):
     list_display = ['title', 'location', 'radius', 'topic', 'user']
     list_filter = ['topic']
 
+
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email','first_name', 'last_name', 
+    list_display = ['username', 'email', 'first_name', 'last_name',
                     'gender', 'is_staff', 'is_superuser', 'date_joined']
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Topic, TopicAdmin)
