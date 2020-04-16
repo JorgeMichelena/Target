5acf990fe2ca95d58f8f7763a7a0abb687d3b9b3
Jorge Michelena <jorge.michelena@rootstrap.com>
2020-04-16 18:59:43 -0300

    Modify test case according to new TargetFactory

--- targets/tests/test_delete_old_targets.py
+++ targets/tests/test_delete_old_targets.py
@@ -8,30 +8,22 @@ import datetime
 
 class DeleteOldTargetsTest(APITestCase):
     def setUp(self):
-        user = UserFactory()
-        user.save()
-        topic = TopicFactory()
-        topic.save()
-        self.target_today = TargetFactory(user=user, topic=topic)
-        self.target_six_days = TargetFactory(user=user, topic=topic)
-        target_one_week = TargetFactory(user=user, topic=topic)
-        target_two_weeks = TargetFactory(user=user, topic=topic)
         six_days_ago = datetime.date.today() - datetime.timedelta(days=6)
         one_week_ago = datetime.date.today() - datetime.timedelta(days=7)
         two_weeks_ago = datetime.date.today() - datetime.timedelta(days=14)
-        self.target_six_days.save()
-        target_one_week.save()
-        target_two_weeks.save()
+        targets = TargetFactory.create_batch(4)
+        self.target_today = targets[0]
+        self.target_six_days = targets[1]
         self.target_six_days.creation_date = six_days_ago
-        target_one_week.creation_date = one_week_ago
-        target_two_weeks.creation_date = two_weeks_ago
-        self.target_six_days.save()
-        target_one_week.save()
-        target_two_weeks.save()
-        self.target_today.save()
+        targets[2].creation_date = one_week_ago
+        targets[2].save()
+        targets[3].creation_date = two_weeks_ago
+        targets[3].save()
 
     def test_delete_one_week_old_targets(self):
+        number_before_method = Target.objects.count()
         delete_one_week_old_targets()
-        targets = Target.objects.all()
-        self.assertEqual(len(targets), 2)
-        self.assertCountEqual(targets, [self.target_today, self.target_six_days])
+        targets_after_method = Target.objects.all()
+        self.assertEqual(number_before_method, 4)
+        self.assertEqual(len(targets_after_method), 2)
+        self.assertCountEqual(targets_after_method, [self.target_today, self.target_six_days])
