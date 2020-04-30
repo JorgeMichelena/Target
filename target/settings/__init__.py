a40163f76c67b45fe6f75863fbcd181874a1f5f0
Jorge Michelena <jorge.michelena@rootstrap.com>
2020-04-30 18:06:29 -0300

    Change settings structure

---
+++ target/settings/__init__.py
@@ -0,0 +1,9 @@
+import os
+import importlib
+
+from .base import *
+
+ENV_ROLE = os.getenv('ENV_ROLE', 'local')
+environment_settings = importlib.import_module(f'target.settings.{ENV_ROLE}_settings')
+
+globals().update(vars(environment_settings))
